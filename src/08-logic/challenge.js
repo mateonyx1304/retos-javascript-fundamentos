9d38bd5e41c64a2d84e948869c9cbd585231ff17
Mateo Angel <mateonyx1304@gmail.com>
2026-05-05 12:57:27 -0500

    Reto de lógica

--- src/08-logic/challenge.js
+++ src/08-logic/challenge.js
@@ -10,45 +10,45 @@
 // Recibe dos booleanos "a" y "b".
 // Retorna el resultado de a && b (true solo si ambas son true).
 function resultadoAnd(a, b) {
-  // Tu código aquí
+return a && b;
 }
 
 // --- Reto 2: OR (||) ---
 // Recibe dos booleanos "a" y "b".
 // Retorna el resultado de a || b (true si al menos una es true).
 function resultadoOr(a, b) {
-  // Tu código aquí
+return a || b;
 }
 
 // --- Reto 3: NOT (!) ---
 // Recibe un booleano "val".
 // Retorna el resultado de !val (invierte: true → false, false → true).
 function resultadoNot(val) {
-  // Tu código aquí
+return !val;
 }
 
 // --- Reto 4: Validación con AND (ambas condiciones) ---
 // Recibe "esAdmin" y "estaActivo" (booleanos).
 // Retorna true solo si es administrador Y está activo (puede ver la lista de usuarios).
 function puedeVerListaUsuarios(esAdmin, estaActivo) {
-  // Tu código aquí
+return esAdmin && estaActivo;
 }
 
 // --- Reto 5: Validación con OR (al menos una condición) ---
 // Recibe "esUsuarioValido" y "tienePermisoEspecial" (booleanos).
 // Retorna true si cumple cualquiera de las dos condiciones (puede acceder).
 function puedeAcceder(esUsuarioValido, tienePermisoEspecial) {
-  // Tu código aquí
+return esUsuarioValido || tienePermisoEspecial;
 }
 
 // --- Reto 6: Validación con NOT (negar condición) ---
 // Recibe "esAdmin" (booleano).
 // Retorna true si NO es administrador (mostrar opción limitada para no admins).
 function mostrarOpcionLimitada(esAdmin) {
-  // Tu código aquí
+return (!esAdmin)
 }
 
-module.exports = {
+export {
   resultadoAnd,
   resultadoOr,
   resultadoNot,
